a4c7acb078f3b1b9df2536bb0944d05caf7926e7
Robesz <kvrobert@gmail.com>
2018-03-18 22:28:20 +0100

    Login refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {AuthService} from "./auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,12 @@ import {AuthService} from "./auth/auth.service";
 })
 export class AppComponent {
 
-  constructor( public auth: AuthService ) {
+  constructor( public auth: AuthService  ) {
     auth.handleAuthentication();    // Handle the authentication
     auth.scheduleRenewal();         // Handle the token refreshing
+
+    if ( !auth.isAuthenticated() ) {
+      auth.login();
+    }
   }
 }
