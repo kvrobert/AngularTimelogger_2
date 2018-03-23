76bc7f7305794a8eb56ade18e43d6a59c6cb62c8
Robert <kvrober@gmail.com>
2018-03-23 15:51:45 +0100

    egy interceptor ötlet hozzáadva...

--- src/app/auth/token-interceptor.ts
+++ src/app/auth/token-interceptor.ts
@@ -32,3 +32,24 @@ export class TokenInterceptor implements HttpInterceptor {
 
   }
 }
+/* // src/app/auth/jwt.interceptor.ts                       ------ ezt lehetne használni majd....
+// ...
+import 'rxjs/add/operator/do';
+export class JwtInterceptor implements HttpInterceptor {
+  constructor(public auth: AuthService) {}
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    
+    return next.handle(request).do((event: HttpEvent<any>) => {
+      if (event instanceof HttpResponse) {
+        // do stuff with response if you want
+      }
+    }, (err: any) => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status === 401) {
+          // redirect to the login route
+          // or show a modal
+        }
+      }
+    });
+  }
+}*/    
